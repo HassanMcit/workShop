49d4001e5018d71b76bd250c0704cb3a3fef3ff7
HassanMcit <hassan.a173784@gmail.com>
2025-01-07 02:01:39 +0200

    fifth commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import logo from '../../assets/logo.jpg'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Navbar() {
 
@@ -22,9 +22,9 @@ export default function Navbar() {
         <div className=" container flex justify-between items-center relative">
           <div className="bg-orange-400 flex justify-between ">
             <div className='w-28'>
-                <Link to="" className='block'>
+                <NavLink to="" className='block'>
                     <img src={logo} alt="Logo" className='w-full' />
-                </Link>
+                </NavLink>
             </div>
             
           </div>
@@ -38,9 +38,9 @@ export default function Navbar() {
           <div className={` fixed z-50 md:static top-0 end-0 ${open ? 'translate-x-0' : 'translate-x-full md:translate-x-0'}  transition-transform duration-300 w-60 md:w-fit`}>
             <button className='text-3xl block md:hidden absolute top-3 end-3 cursor-pointer' onClick={handleToggle}><i className="fa-solid fa-x"></i></button>
             <ul className='bg-white/90 md:bg-transparent h-96 md:h-fit dark:md:text-white flex flex-col md:flex-row space-y-8 p-10 md:p-0  md:space-y-0 md:space-x-7 text-3xl'>
-                <li><Link to="/home" className='block'>Home</Link></li>
-                <li><Link to="/about" className='block'>About</Link></li>
-                <li><Link to="/contact" className='block'>Contact</Link></li>
+                <li><NavLink to="/home" className='block py-3'>Home</NavLink></li>
+                <li><NavLink to="/about" className='block py-3'>About</NavLink></li>
+                <li><NavLink to="/contact" className='block py-3'>Contact</NavLink></li>
             </ul>
           </div>
         </div>
